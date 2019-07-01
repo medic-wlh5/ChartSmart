c3ee01e5e13fa721d897a4e482b75b5f8d734aef
Rory Markham <rorymarkham@Rorys-MacBook-Pro.local>
2019-07-01 16:49:16 -0600

    refactored controller folder

--- server/Controllers/authcontroller.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-module.exports = {
-    doctorRegister: async (req, res) => {},
-	patientRegister: async (req, res) => {},
-	doctorLogin: async (req, res) => {},
-	patientLogin: async (req, res) => {},
-	updateUser: async (req, res) => {},
-	logout: (req, res) => {},
-};

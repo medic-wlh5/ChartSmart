0d878fe51c19bb6f8b58758185d7bfbeaa37fc2f
Jeremyegan <jeremyegan1@gmail.com>
2019-07-01 15:15:53 -0600

    skeleton DocAuth waiting for pull

--- src/Components/DocAuth/DocAuth.js
+++ src/Components/DocAuth/DocAuth.js
@@ -1,5 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class DocAuth extends Component {
-    
+    constructor() {
+        super()
+        this.state = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: '',
+            pin: '',
+            office: ''
+        }
+    }
+
+
+    render() {
+        return(
+            <div>
+                <form>
+                    <input placeholder="First Name"></input>
+                    <input placeholder="Last Name"></input>
+                    <input placeholder="Email"></input>
+                    <input placeholder="Passwork"></input>
+                    <input placeholder="4 Digit Pin"></input>
+                    <input placeholder="Office"></input>
+                    <button></button>
+                </form>
+            </div>
+        )
+    }
 }

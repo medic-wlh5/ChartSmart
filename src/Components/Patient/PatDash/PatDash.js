178140a767e43be32256d83bd5f5dfd0a63b6843
Rory Markham <rorymarkham@Rorys-MacBook-Pro.local>
2019-07-02 11:42:44 -0600

    feature: set up patient login and registration

--- src/Components/Patient/PatDash/PatDash.js
+++ src/Components/Patient/PatDash/PatDash.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {updateUser} from '../../../redux/patientReducer'
 
 export default class PatDash extends Component {
     render() {
--- src/redux/patientReducer.js
+++ src/redux/patientReducer.js
@@ -1,3 +1,25 @@
 const initialState = {
-    
+    first_name: '',
+    last_name: '',
+    email: '',
+    dob: '',
+    id: '',
+}
+
+const UPDATE_USER = 'UPDATE_USER'
+
+export function updateUser(user){
+    return {
+        type: UPDATE_USER,
+        payload: user
+    }
+}
+
+export default function reducer(state = initialState, action){
+    switch (action.type){
+        case UPDATE_USER:
+        const {first_name, last_name, email, dob, id} = action.payload
+        return {...state, first_name, last_name, email, dob, id}
+        default: return state
+    }
 }

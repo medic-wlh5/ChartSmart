b2165e0d8aea4172dad853b69f289959133ae6c4
Jeremyegan <jeremyegan1@gmail.com>
2019-07-02 11:31:23 -0600

    completed register

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import patientReducer from './patientReducer';
 import doctorReducer from './doctorReducer';
 

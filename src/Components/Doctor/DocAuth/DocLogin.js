26ea4a9d5112ebd4af48f876346cfc9d4e9f289c
Jeremyegan <jeremyegan1@gmail.com>
2019-07-02 12:24:40 -0600

    feat: completed doctor login/logout

--- src/Components/Doctor/DocAuth/DocLogin.js
+++ src/Components/Doctor/DocAuth/DocLogin.js
@@ -1,11 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { clearDoctor } from "../../../redux/doctorReducer";
+import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 
-export default class DocLogin extends Component {
-    render() {
-        return(
-            <div>
-                <h1>DocLogin</h1>
-            </div>
-        )
-    }
+class DocLogin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChange = (e) => {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    axios
+      .post("/auth/doctorlogin", { email, password })
+      .then(res => {
+        this.props.history.push("/doctordashboard");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    // e.target.email.value = "";
+    // e.target.password.value = "";
+  };
+
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input type="email" name="email" placeholder="email" onChange={this.handleChange}/>
+          <input type="password" name="password" placeholder="password" onChange={this.handleChange}/>
+          <button>Login</button>
+        </form>
+        <Link to="/doctorregister"><button>Register</button></Link>
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(reduxState) {
+  return reduxState;
+}
+
+export default connect(
+  mapStateToProps,
+  { clearDoctor }
+)(DocLogin);
--- src/Components/Doctor/DocDash/DocDash.js
+++ src/Components/Doctor/DocDash/DocDash.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { clearDoctor } from "../../../redux/doctorReducer";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default class DocDash extends Component {
-    render() {
-        return (
-            <div>
-                <h1>DocDash</h1>
-            </div>
-        )
-    }
+class DocDash extends Component {
+  constructor() {
+    super();
+  }
+
+  handleLogout = () => {
+    this.props.clearDoctor();
+    axios.get("/auth/logout").catch(err => {
+      console.log(err);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>DocDash</h1>
+        <Link to="/">
+          <button onClick={this.handleLogout}>Logout</button>
+        </Link>
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(reduxState) {
+  return reduxState;
+}
+
+export default connect(
+  mapStateToProps,
+  { clearDoctor }
+)(DocDash);

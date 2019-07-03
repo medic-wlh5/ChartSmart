be91c5c0b48fca63e79d4ac443fec66e4e9245c0
Jeremyegan <jeremyegan1@gmail.com>
2019-07-03 14:04:48 -0600

    feat: added components in PatChart

---
+++ src/Components/Patient/PatCharts/Vitals/Vitals.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Vitals extends Component {
+    render() {
+        return (
+            <div>
+                <h1> vitals placeholder </h1>
+            </div>
+        )
+    }
+}

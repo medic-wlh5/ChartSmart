ce5321e25390b03809a5d444fabfc7040a16519e
Rory Markham <rorymarkham@Rorys-MacBook-Pro.local>
2019-07-03 16:05:18 -0600

    staging changes

--- src/Components/Patient/PatientAuthentication/PatientRegister.js
+++ src/Components/Patient/PatientAuthentication/PatientRegister.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import '../PatForm.css'
 // import {withRouter} from 'react-router-dom'
 
 class PatientRegister extends Component {

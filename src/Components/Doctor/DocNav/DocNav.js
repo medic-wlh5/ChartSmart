e5e36f61437a51df0fe88b72aade08a4729656e7
Rory Markham <rorymarkham@Rorys-MacBook-Pro.local>
2019-07-05 11:40:48 -0600

    styling changes

--- src/Components/Doctor/DocNav/DocNav.js
+++ src/Components/Doctor/DocNav/DocNav.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
+import './DocNav.css'
+import {Link} from 'react-router-dom'
 
 //Icons
-import Logo from './Assets/IconTrans2.png'
-import Chart from './Assets/ChartTrans2.png'
-import Calender from './Assets/CalTrans2.png'
-import Chat from './Assets/ChatTrans2.png'
-import Profile from './Assets/ProfTrans2.png'
+import Logo from '../../../Assets/IconTrans2.png'
+import Chart from '../../../Assets/ChartTrans2.png'
+import Calender from '../../../Assets/CalTrans2.png'
+import Chat from '../../../Assets/ChatTrans2.png'
+import Profile from '../../../Assets/ProfTrans2.png'
+
 
 export default class DocNav extends Component {
     render() {
         return (
             <div>
                 <div className='nav_menu'>
+                        <Link>
                         <button className='icons'><img src={Logo} height='90px' width='100px'></img></button>
+                        </Link>
                         <button className='icons'><img src={Chart} height='60px' width='70px'></img></button>
                         <button className='icons'><img src={Calender} height='60px' width='70px'></img></button>
                         <button className='icons'><img src={Chat} height='60px' width='70px'></img></button>

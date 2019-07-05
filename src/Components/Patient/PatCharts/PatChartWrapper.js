e201508a602b443b5f62c00179edfa96c30ead93
teastonsmith2 <t.easton.smith2@gmail.com>
2019-07-05 17:01:09 -0600

    feat: added button to render white blood cell chart

--- src/Components/Patient/PatCharts/PatChartWrapper.js
+++ src/Components/Patient/PatCharts/PatChartWrapper.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import WhiteBloodCell from './Bloodwork/WhiteBloodCell';
 
-
-
-
 export default class PatChartWrapper extends Component {
-
-    
-    render() {
-        return (
-            <div>
-                <WhiteBloodCell />
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+            whiteMenu: false,
+            test: ''
+		};
+	}
+	showWhite = () => {
+		this.setState({
+			whiteMenu: true,
+		});
+	};
+	handleTestTypeChange = e => {
+		this.setState({ test: e.target.value });
+	};
+	render() {
+        console.log(this.state);
+        
+		return (
+			<div>
+				<form>
+					<label>
+						Choose test type:
+						<select
+							value={this.state.test}
+							onChange={this.handleTestTypeChange}>
+							<option value='' />
+							<option value='whiteBloodCellCount'>White Blood Cell Count</option>
+							<option value='vitals'>Vitals</option>
+						</select>
+					</label>
+				</form>
+				<div className='chartContainer'>
+					<button className='whiteBloodCell' onClick={this.showWhite}>
+						White Blood Cell Menu
+					</button>
+				</div>
+				<div>{this.state.whiteMenu ? <WhiteBloodCell /> : null}</div>
+			</div>
+		);
+	}
 }
-

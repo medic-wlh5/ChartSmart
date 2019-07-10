f57aacee205e7ae2cfa1afd36632f1f9c6e49af5
Jeremyegan <jeremyegan1@gmail.com>
2019-07-10 11:34:28 -0600

    pull to commit

---
+++ src/Components/Message/Message.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Message extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

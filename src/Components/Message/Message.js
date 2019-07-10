ce90215ae3d51a0267bf04c7bc3c5153be4fc1c4
Jeremyegan <jeremyegan1@gmail.com>
2019-07-10 16:57:20 -0600

    commit to pull

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import ReactContactForm from 'react-mail-form';
+import PatNav from '../Patient/PatNav/PatNav';
+import './Message.css';
  
 
 
 
 export default class Message extends Component {
+    
     render() {
+        // const emailStyle = {
+        //     display: 'flex',
+        //     flexDirection: 'column'
+        // }
         return (
-            <div>
-                <ReactContactForm to="address@gmail.com" />
+            <div className="page-message">
+                <PatNav />
+                <div className="email-form">
+                <ReactContactForm to="address@gmail.com" className="react-contact-form" />
+                </div>
             </div>
         )
     }

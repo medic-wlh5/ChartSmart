854d16bd163d343c37f0247684ed2fa530717214
Jeremyegan <jeremyegan1@gmail.com>
2019-07-10 15:59:12 -0600

    commit to pull

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import ReactContactForm from 'react-mail-form';
+ 
+
 
 
 export default class Message extends Component {
     render() {
         return (
             <div>
-                
+                <ReactContactForm to="address@gmail.com" />
             </div>
         )
     }
